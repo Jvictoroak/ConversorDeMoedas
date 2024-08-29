85689510b2fb82c8b46af8d9b9a41a52d24d3078
João Victor Carvalho <joaovictor.crvl@gmail.com>
2024-08-29 12:18:49 -0300

    criação do formulario

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ async function convertCurrency(from, to, amount) {
     try {
         // Faz uma requisição HTTP GET para a URL da API
         const response = await fetch(API_URL);
-        
+
         // Converte a resposta da API para um objeto JavaScript (JSON)
         const data = await response.json();
 
         // A chave no objeto JSON é uma combinação das moedas de origem e destino (ex: "USDBRL")
         const key = `${from}${to}`;
-        
+
         // Obtém a taxa de câmbio da resposta da API. "bid" representa o preço de venda da moeda de origem para a moeda de destino
         const rate = parseFloat(data[key].bid);
 
@@ -30,28 +30,29 @@ async function convertCurrency(from, to, amount) {
     } catch (error) {
         // Se houver um erro ao acessar a API, ele será capturado aqui
         console.error('Erro ao acessar a AwesomeAPI:', error);
-        
+
         // Retorna null em caso de erro
         return null;
     }
 }
 
 // Exemplo de uso da função, executado imediatamente
+function converter() {
+    (async () => {
+        moeda = document.getElementById("moeda").value
+        moedaConvertida = document.getElementById("moedaConvertida").value
+        valor = document.getElementById("valor").value
+        // Chama a função convertCurrency para converter 100 USD para BRL
+        const result = await convertCurrency(moeda, moedaConvertida, valor);
+        // const result = await convertCurrency('USD', 'BRL', 100);
 
-(async function converter(){
-    moeda = document.getElementById("moeda").value
-    moedaConvertida = document.getElementById("moedaConvertida").value
-    valor = document.getElementById("valor").value
-    // Chama a função convertCurrency para converter 100 USD para BRL
-    const result = await convertCurrency(moeda, moedaConvertida, valor);
-    // const result = await convertCurrency('USD', 'BRL', 100);
-
-    // Verifica se a conversão foi bem-sucedida
-    if (result) {
-        // Se sim, imprime os detalhes da conversão no console
-        console.log(`Converteu ${result.amount} ${result.from} para ${result.convertedAmount} ${result.to} com a taxa de câmbio de ${result.rate}`);
-    } else {
-        // Se não, imprime uma mensagem de erro
-        console.log('Erro ao realizar a conversão.');
-    }
-})();
+        // Verifica se a conversão foi bem-sucedida
+        if (result) {
+            // Se sim, imprime os detalhes da conversão no console
+            console.log(`Converteu ${result.amount} ${result.from} para ${result.convertedAmount} ${result.to} com a taxa de câmbio de ${result.rate}`);
+        } else {
+            // Se não, imprime uma mensagem de erro
+            console.log('Erro ao realizar a conversão.');
+        }
+    })();
+}
